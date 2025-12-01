e5fecf9bf3b4b9f528a8f377ca699fa36494e88d
Youssef-Ahmed-Zahran <youssefzahran480@gmail.com>
2025-12-01 14:42:22 +0200

    fix deploy

--- backend/server.js
+++ backend/server.js
@@ -24,8 +24,7 @@ app.use(helmet());
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    // TODO: Add your deployed frontend URL here after deployment
-    // "https://your-frontend-app.vercel.app"
+    "https://linkedin-clone-frontend-tau.vercel.app",
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
